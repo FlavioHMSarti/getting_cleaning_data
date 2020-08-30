14f62f8eaaed844a107f1e110ac48af642e9ce66
Flavio_PC <flavio.hmsarti@gmail.com>
2020-08-30 03:23:28 +0200

    merging data

--- run_analysis.R
+++ run_analysis.R
@@ -12,9 +12,23 @@ setwd(repository)
 repository_train <- paste(repository, "/train", sep = "")
 repository_test <- paste(repository, "/test", sep = "")
 
-files_train <- list.files(repository_train)
-files_test <- list.files(repository_test)
+data_feature <- read.table("features.txt")
 
-setwd(repository_train)
+files_train <- list.files(repository_train, pattern = "txt", full.names = TRUE)
+files_test <- list.files(repository_test, pattern = "txt", full.names = TRUE)
+
+
 data_train <- sapply(files_train, read.table, simplify = FALSE)
 data_test <- sapply(files_test, read.table, simplify = FALSE)
+
+data_both <- mapply(rbind, data_train, data_test)
+data_both <- do.call(cbind, data_both)
+
+names(data_both)[1] <- "candidate"
+names(data_both)[length(data_both)] <- "activity"
+
+setnames(data_both, c(2:(length(data_both)-1)),data_feature[[2]])
+
+mean_values_pos <- grep("mean|std", data_feature[[2]])+1
+mean_values <- data_both[,c(1,mean_values_pos,length(data_both))]
+
